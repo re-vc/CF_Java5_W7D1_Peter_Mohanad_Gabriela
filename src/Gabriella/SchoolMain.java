60665bce3c3c6c197174ef1953ca398ffee00e4b
gabriella.kecskemeti <gabriella.kecskemeti@chello.at>
2020-12-07 14:30:54 +0100

    it works

--- src/Gabriella/SchoolMain.java
+++ src/Gabriella/SchoolMain.java
@@ -1,5 +1,6 @@
 package Gabriella;
 import java.sql.*;
+import java.util.*;
 
 public class SchoolMain {
     // JDBC driver name and database URL
@@ -21,11 +22,33 @@ public class SchoolMain {
             System.out.println("Connecting to database...");
             con = DriverManager.getConnection(url,user,password);
 
+            //************************add student data******************
+             System.out.println("Student name:");
+             Scanner in = new Scanner(System.in);
+             String sName = in.next();
+
+             System.out.println("Student address:");
+
+             String sAddress = in.next();
+
+             System.out.println("add data into sql db...");
+
+             String sql4;
+             sql4 = "insert into students (name, address) values (?,?);";
+            PreparedStatement preparedStatement1 = con.prepareStatement(sql4);
+
+            preparedStatement1.setString(1, sName);
+            preparedStatement1.setString(2, sAddress);
+
+            preparedStatement1.executeUpdate();
+
+
+
             //STEP 4: Execute a query
             System.out.println("Creating our statement...");
             Statement statement = con.createStatement();
             String sql;
-            sql = "SELECT * FROM students";
+            sql = "SELECT * FROM students order by name";
             ResultSet rs = statement.executeQuery(sql);
 
             //STEP 5: Extract data from result set
@@ -41,6 +64,8 @@ public class SchoolMain {
                 System.out.print(", Name: " + name);
                 System.out.println(", Address: " + address);
             }
+            rs.close();
+            statement.close();
             //***************************second table
             //STEP 4: Execute a query
             System.out.println("Creating our statement2...");
@@ -61,13 +86,45 @@ public class SchoolMain {
                 System.out.println(", Name: " + name2);
 
             }
-            //**********************end second table
-
-            //STEP 6: Clean-up environment
-            rs.close();
-            statement.close();
             rs2.close();
             statement2.close();
+            //**********************end second table
+
+            //***************************third quiry
+            //STEP 4: Execute a query
+            System.out.println("Creating our statement3...");
+            Statement statement3 = con.createStatement();
+            String sql3;
+            sql3 = "SELECT enrollments.fk_students_id, students.name, enrollments.fk_courses_id, courses.name from enrollments " +
+                    "join students on students.id=enrollments.fk_students_id" +
+                    " join courses on courses.id=enrollments.fk_courses_id";
+            ResultSet rs3 = statement3.executeQuery(sql3);
+
+            //STEP 5: Extract data from result set
+            while(rs3.next()){
+                //Retrieve by column name
+                int sId3  = rs3.getInt("enrollments.fk_students_id");  //add the field name from DB table
+                String sName3 = rs3.getString("students.name");
+                int cId3  = rs3.getInt("enrollments.fk_courses_id");  //add the field name from DB table
+                String cName3 = rs3.getString("courses.name");
+
+                //Display values
+                System.out.print("Student ID: " + sId3);
+                System.out.print(", Name: " + sName3);
+                System.out.print("    Course ID: " + cId3);
+                System.out.println(", Name: " + cName3);
+
+            }
+            //STEP 6: Clean-up environment
+            rs3.close();
+            statement3.close();
+            //**********************end third quiry
+
+
+
+
+
+
         }catch(SQLException se){
             //Handle errors for JDBC
             se.printStackTrace();
